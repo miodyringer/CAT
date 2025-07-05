fab19c2edf3b7655ca979ed04cdcc6f10f433182
Finn Renzenbrink <finn.renzenbrink@sap.com>
2025-07-05 12:40:37 +0200

    api to select page

---
+++ CAT/API/pages_connection_api.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Statische Verzeichnisse einbinden
+app.mount("/stylesheets", StaticFiles(directory="CAT/stylesheets"), name="stylesheets")
+app.mount("/scripts", StaticFiles(directory="CAT/scripts"), name="scripts")
+
+
+@app.get("/about", response_class=HTMLResponse)
+async def get_about():
+    with open("CAT/pages/about.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/create_lobby", response_class=HTMLResponse)
+async def get_create_lobby():
+    with open("CAT/pages/create_lobby.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/game", response_class=HTMLResponse)
+async def get_game():
+    with open("CAT/pages/game.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/join_lobby", response_class=HTMLResponse)
+async def get_join_lobby():
+    with open("CAT/pages/join_lobby.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/menu", response_class=HTMLResponse)
+async def get_menu():
+    with open("CAT/pages/menu.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/online", response_class=HTMLResponse)
+async def get_online():
+    with open("CAT/pages/online.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/rules", response_class=HTMLResponse)
+async def get_rules():
+    with open("CAT/pages/rules.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
+
+@app.get("/settings", response_class=HTMLResponse)
+async def get_settings():
+    with open("CAT/pages/settings.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
